20321e861e7f28c048f7eade7df0a090d549b0a8
zlessner <LessnerZachary@gmail.com>
2020-05-26 11:36:38 -0400

    added math questions

---
+++ Quiz/questions.py
@@ -0,0 +1,11 @@
+capital_questions = {
+    'Nigeria': 'Abuja',
+    'Iran': 'Tehran',
+    'Vietnam': 'Hanoi',
+    'Canada': 'Ottawa',
+    'Australia': 'Canberra'}
+
+math_questions = {
+    '11x12+13': '145',
+    '12*23': '276'
+}
--- Quiz/quiz.py
+++ Quiz/quiz.py
@@ -1,29 +1,57 @@
-# create class of questions
-# create function
-
 import random, copy
+from questions import capital_questions, math_questions
 
-capital_questions = {
-    'Nigeria': 'Abuja',
-    'Iran': 'Tehran',
-    'Vietnam': 'Hanoi'}
-
-keys = list(capital_questions.keys())
-
-random.shuffle(keys)
+class Questions:
+    def __init__(self, capital_questions, math_questions):
+        self.capital_questions = capital_questions
+        self.math_questions = math_questions
 
 
-shuffled_capital_questions = dict()
-for key in keys:
-    shuffled_capital_questions.update({key:capital_questions[key]})
+
+        cap_keys = list(self.capital_questions.keys())
+        math_keys = list(self.math_questions.keys())
+
+        random.shuffle(cap_keys)
+        random.shuffle(math_keys)
 
 
-print('''
-What is the capital of {country}? '''.format(country=next(iter(shuffled_capital_questions))))
+        shuffled_capital_questions = dict()
+        shuffled_math_questions = dict()
 
-answer = input() 
+        for key in cap_keys:
+            shuffled_capital_questions.update({key:self.capital_questions[key]})
+        for key in math_keys:
+            shuffled_math_questions.update({key:self.math_questions[key]})
 
-if answer == shuffled_capital_questions.get(next(iter(shuffled_capital_questions))):
-    print ("You are correct!")
-else:
-    print ("Sorry, that answer is incorrect")
+        
+        ranNumGen = random.random()
+
+        if ranNumGen < .5:
+            print('''
+            What is the capital of {country}? '''.format(country=next(iter(shuffled_capital_questions))))
+
+            user_input = input() 
+
+            if user_input == shuffled_capital_questions.get(next(iter(shuffled_capital_questions))):
+                print ("You are correct!")
+            else:
+                print ("Sorry, that answer is incorrect. The capital of {question} is {answer}.".
+                format(question=next(iter(shuffled_capital_questions)), 
+                answer=shuffled_capital_questions.get(next(iter(shuffled_capital_questions)))))
+
+        else:
+            print(next(iter(shuffled_math_questions)))
+
+            user2_input = input()
+
+            if user2_input == shuffled_math_questions.get(next(iter(shuffled_math_questions))):
+                print ("You are correct!")
+            else:
+                print ("Sorry, that answer is incorrect. {question}={answer}.".
+                format(question=next(iter(shuffled_math_questions)), 
+                answer=shuffled_math_questions.get(next(iter(shuffled_math_questions)))))
+
+
+
+
+run = Questions(capital_questions, math_questions)
