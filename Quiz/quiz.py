396012303fd67960d8e72b8b024c633e2ae10fd0
zlessner <LessnerZachary@gmail.com>
2020-05-26 12:35:43 -0400

    Consolidating code

--- Quiz/quiz.py
+++ Quiz/quiz.py
@@ -1,3 +1,6 @@
+#consolidate code
+#make flask front end
+
 import random, copy
 from questions import capital_questions, math_questions
 
@@ -6,51 +9,45 @@ class Questions:
         self.capital_questions = capital_questions
         self.math_questions = math_questions
 
-
-
-        cap_keys = list(self.capital_questions.keys())
-        math_keys = list(self.math_questions.keys())
-
-        random.shuffle(cap_keys)
-        random.shuffle(math_keys)
-
-
-        shuffled_capital_questions = dict()
-        shuffled_math_questions = dict()
-
-        for key in cap_keys:
-            shuffled_capital_questions.update({key:self.capital_questions[key]})
-        for key in math_keys:
-            shuffled_math_questions.update({key:self.math_questions[key]})
-
-        
         ranNumGen = random.random()
+        question_value = dict()
 
         if ranNumGen < .5:
-            print('''
-            What is the capital of {country}? '''.format(country=next(iter(shuffled_capital_questions))))
-
-            user_input = input() 
-
-            if user_input == shuffled_capital_questions.get(next(iter(shuffled_capital_questions))):
-                print ("You are correct!")
-            else:
-                print ("Sorry, that answer is incorrect. The capital of {question} is {answer}.".
-                format(question=next(iter(shuffled_capital_questions)), 
-                answer=shuffled_capital_questions.get(next(iter(shuffled_capital_questions)))))
+            category = self.capital_questions
+            answer_value = "Sorry, that answer is incorrect. The capital of {question} is {answer}."
 
         else:
-            print(next(iter(shuffled_math_questions)))
+            category = self.math_questions
+            answer_value = "Sorry, that answer is incorrect. {question}={answer}."
 
-            user2_input = input()
 
-            if user2_input == shuffled_math_questions.get(next(iter(shuffled_math_questions))):
-                print ("You are correct!")
-            else:
-                print ("Sorry, that answer is incorrect. {question}={answer}.".
-                format(question=next(iter(shuffled_math_questions)), 
-                answer=shuffled_math_questions.get(next(iter(shuffled_math_questions)))))
+        keys = list(category.keys()) 
 
+        random.shuffle(keys)
+
+        for key in keys:
+            question_value.update({key:category[key]})
+
+
+        #Capital Questions
+        if ranNumGen < .5:
+            print('''
+            What is the capital of {country}? '''.format(country=next(iter(question_value))))
+
+        #Math Questions
+        else:
+            print(next(iter(question_value)))
+
+        user_input = input() 
+
+        if user_input.lower() == question_value.get(next(iter(question_value))).lower():
+            print ("You are correct!")
+
+        #Answer is based on what type of question it is    
+        else:
+            print (answer_value.
+            format(question=next(iter(question_value)), 
+            answer=question_value.get(next(iter(question_value)))))
 
 
 
